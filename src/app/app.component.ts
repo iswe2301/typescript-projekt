58f4d9e9433e2dc2e5d702fc8467fa3e7fd03ab6
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-27 12:11:27 +0200

    Importerat navigationskomponenten

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  // Importerar navigationskomponenten
+  imports: [RouterOutlet, NavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
