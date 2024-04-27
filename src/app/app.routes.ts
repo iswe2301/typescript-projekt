72bade0bfe5de32ab20c08db744a864328614aae
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-27 11:57:50 +0200

    Definierat rutter för applikationen

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { SchemeComponent } from './scheme/scheme.component';
+import { CoursesComponent } from './courses/courses.component';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-export const routes: Routes = [];
+// Applikationens rutter
+export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
+    { path: 'courses', component: CoursesComponent },
+    { path: 'scheme', component: SchemeComponent },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: '404', component: NotFoundComponent },
+    { path: '**', component: NotFoundComponent }
+];
