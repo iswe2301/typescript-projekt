ca4a621f09ee50dacd5cbd14ae44b0e8e8d4c10b
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-27 16:17:59 +0200

    Skapat metoder för att hämta kurser från localStorage och läsa ut sparade kurser. Lagt till load-metod i konstruktorn. Skapat metod för att hämta totala HP i ramschemat.

--- src/app/services/scheme.service.ts
+++ src/app/services/scheme.service.ts
@@ -10,6 +10,8 @@ export class SchemeService {
   scheme: Course[] = []; // Array för att lagra kurser i ramschemat, initeras till tom array av typen interfacet Course
 
   constructor() {
+    // Läser in in ramschemat från localStorage vid start
+    this.loadSchedule();
   }
 
   // Metod för att lägga till en kurs till ramschemat
@@ -20,9 +22,42 @@ export class SchemeService {
     this.saveSchedule();
   }
 
+  // Metod för att hämta kurserna i ramschemat
+  getSchedule(): Course[] {
+    // Hämtar ramschemat från localStorage
+    const storedSchedule = localStorage.getItem("scheme");
+    // Kontrollerar om ramschemat finns i localStorage
+    if (storedSchedule) {
+      // Returnerar det sparade ramschemat om det finns
+      return JSON.parse(storedSchedule);
+    } else {
+      // Annars returneras en tom array
+      return [];
+    }
+  }
+
+  // Metod för att hämta totala antalet högskolepoäng i ramschemat
+  getTotalPoints(): number {
+    let totalPoints = 0; // Initierar den totala poängen till 0
+    // Loopar igenom schemat
+    for (let i = 0; i < this.scheme.length; i++) {
+      totalPoints += this.scheme[i].points; // Lägger till poängen för varje kurs till den totala summan
+    }
+    return totalPoints; // Returnerar den totala summan av poängen
+  }
+
   // Metod för att spara ramschemat i localStorage
   saveSchedule(): void {
     // Sparar ramschemat under nyckeln "scheme"
     localStorage.setItem("scheme", JSON.stringify(this.scheme));
+  }
+
+  // Metod för att läsa in ramschemat från localStorage
+  loadSchedule(): void {
+    // Hämtar ramschemat från localStorage
+    const storedSchedule = localStorage.getItem("scheme");
+    if (storedSchedule) {
+      this.scheme = JSON.parse(storedSchedule);
+    }
   }
 }
