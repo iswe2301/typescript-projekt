6c64399296ba1a6d4a1d8cd9272a86ca234661b3
Isa Westling <isawestling.blogg@hotmail.com>
2024-04-28 20:21:22 +0200

    Skapat mobilmeny + styling och TS-funktionalitet

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -10,5 +10,16 @@ import { RouterLink, RouterLinkActive } from '@angular/router';
   styleUrl: './navigation.component.scss'
 })
 export class NavigationComponent {
+  // Skapat en egenskap av typen boolean för att definiera mobilmenyns synlighet, sätter initalt till false (dold)
+  isVisible: boolean = false;
 
+  // Metod för att visa/dölja menyn, växlar värdet på isVisible som anropas när användaren klickar på menyknappen
+  toggleMenu(): void {
+    this.isVisible = !this.isVisible; // Väclar mellan true och false
+  }
+
+  // Metod för att dölja menyn vid klick på menylänk eller overlay
+  hideMenu(): void {
+    this.isVisible = false;
+  }
 }
