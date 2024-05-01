6a9f66453a393d793ce49cc4c6540a96ad1c6c0b
Isa Westling <isawestling.blogg@hotmail.com>
2024-05-01 17:56:41 +0200

    Installerat angular material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,8 +3,9 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 // Importerar provideHttpClient för att få tillgång från hela applikationen
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient()]
+  providers: [provideRouter(routes), provideHttpClient(), provideAnimationsAsync()]
 };
