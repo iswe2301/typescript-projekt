bbb0a070954f81f99ebeda7dc0558371d24d6d33
Isa Westling <isawestling.blogg@hotmail.com>
2024-05-02 17:09:39 +0200

    Skapt interface för styling av kursämnen

---
+++ src/app/models/subjectstyle.ts
@@ -0,0 +1,8 @@
+// Interface för att definiera strukturen av styling för ett kursämne
+export interface SubjectStyle {
+    "font-size": string;
+    "font-weight": string;
+    "transform": string;
+    "display": string;
+    "margin": string;
+}
